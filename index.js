38180bfffe59dc28513b3ecc00bd45a4a50a9e70
Dustin <dustin@vultr.guest>
2018-04-16 14:31:54 +0000

    fix type in script

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ concat({
   output: 'test.mp4',
   videos: [
     'cut1.mp4',
-    'cut22mp4'
+    'cut2.mp4'
   ],
   transition: {
     name: 'directionalWipe',
     duration: 500
   }
+}).then(() => {
+  console.log('done')
+}, (err) => {
+  console.error('error', err)
 })
 
